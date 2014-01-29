e5453fc0243de560d523e4f027ab4ce2328d985e
Airthur05 <airzaayamaha@gmail.com>
2014-01-29 18:20:19 +0700

    NewVersion

--- Lab11_1.php
+++ Lab11_1.php
@@ -1,6 +1,7 @@
 <?php
       phpinfo();
 	  echo '<p>Hello World</p>';
+	  echo '<p>My name is Sarawut Sattapon/p>';
 	  echo '<p>What Wonderful day</p>';
 	  echo '<p>We are having fun</p>';
 	  ?>
